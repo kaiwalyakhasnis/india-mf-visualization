606882db9ca1a1ed669ad53cc737b490946e8fc3
kaiwalya khasnis <kaiwalya.khasnis@rakuten.com>
2022-04-11 22:09:23 +0900

    basic implementation for report generation

--- core/main.py
+++ core/main.py
@@ -1,2 +1,41 @@
+from functools import reduce
+import os
+
+import numpy as np
+import pandas as pd
+from core.transformers.NavNotnaTransformer import NavNotnaTransformer
+from core.validators.ColumnDatatypeValidation import ColumnDatatypeValidation
+from core.validators.ColumnNameValidation import ColumnNameValidation
+
+validators = [
+    ColumnNameValidation(),
+    # ColumnDatatypeValidation()
+]
+
+transformers = [
+    NavNotnaTransformer()
+]
+
+
+def execute(files):
+    report_df = pd.DataFrame(data={})
+
+    for file in files:
+        print(file)
+        df = pd.read_csv(f"../data/{file}", sep=';')
+        df = df.replace(to_replace='N.A', value=np.nan, regex=True)
+        if all(validator.validate(df) for validator in validators):
+            report_df = pd.concat(
+                [report_df,
+                 reduce(
+                     lambda result, transformer: transformer.transform(result),  # function
+                     transformers,  # sequence
+                     df)]  # initial
+            )
+        else:
+            print(f"{file} has issue")
+    report_df.to_csv("../output/report.csv")
+
+
 if __name__ == '__main__':
-    pass
+    execute(os.listdir("../data"))
--- test/core/validators/test_ColumnDatatypeValidation.py
+++ test/core/validators/test_ColumnDatatypeValidation.py
@@ -56,3 +56,21 @@ class TestColumnDatatypeValidation(TestCase):
 
         # act and assert
         self.assertTrue(ColumnDatatypeValidation().validate(test_pd))
+
+    def test_validate_nav_string_na_value(self):
+        # arrange
+        test_pd = pd.DataFrame(data={
+            Column.SCHEME_CODE.value: [100612, 100613],
+            Column.SCHEME_NAME.value: ["Sundaram Growth Fund-Dividend 1", "Sundaram Growth Fund-Dividend 2"],
+            Column.ISIN_PAYOUT.value: ["INF903J01330", "INF903J01331"],
+            Column.ISIN_DIV_REINVEST.value: ["INF903J01348", "INF903J01348"],
+            Column.NAV.value: ["N.A", "N.A"],
+            Column.REPURCHASE_PRICE.value: [100.0, np.nan],
+            Column.SALE_PRICE.value: [3000.0, 6000.0],
+            Column.DATE.value: ["01-Apr-2006", "01-Apr-2006"]
+        })
+
+        test_pd = test_pd.replace({"N.A": np.nan})
+
+        # act and assert
+        self.assertTrue(ColumnDatatypeValidation().validate(test_pd))
